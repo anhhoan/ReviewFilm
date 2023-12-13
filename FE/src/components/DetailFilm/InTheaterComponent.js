25262abc888cbba6d0b8a5fc273706a03a1e1a53
unknown <anhhoan>
2023-12-13 16:44:58 +0700

    first commit

---
+++ FE/src/components/DetailFilm/InTheaterComponent.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import '../../styles/css/ListFilm.css';
+
+export default class InTheaterComponent extends Component {
+  render() {
+    const filmData = this.props.filmDetail ? this.props.filmDetail.Cinemas : [];
+    return (
+      <div className="in-theater">
+        <h2>Rạp Chiếu</h2>
+        <div className="theater">
+          {filmData &&
+            filmData.map((item, key) => (
+              <div key={key}> {item.CinemaClusterName}</div>
+            ))}
+        </div>
+      </div>
+    );
+  }
+}
